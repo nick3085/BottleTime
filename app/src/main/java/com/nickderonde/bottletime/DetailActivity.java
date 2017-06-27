c2e277c1ac4fb7e15576796f8047c71ac91c47b5
Nick de Ronde <nick3085@gmail.com>
2017-06-27 18:44:31 +0200

    Settings begin, detail view finished and maps start

--- app/src/main/java/com/nickderonde/bottletime/DetailActivity.java
+++ app/src/main/java/com/nickderonde/bottletime/DetailActivity.java
@@ -2,27 +2,45 @@ package com.nickderonde.bottletime;
 
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
+import java.text.DecimalFormat;
 import java.util.Map;
 
 public class DetailActivity extends AppCompatActivity {
 
     private TextView mProductTitleTV;
     private TextView mProductProducerTV;
-    private Image mProductImageUrlIm;
-    private Button mProductLoactionBtn;
+    private ImageView mProductImageUrlIm;
+    private TextView mPriceInCentsTV;
+    private TextView mAlcoholContentTV;
+    private TextView mPrimaryCategoryTV;
+    private TextView mSecondaryCategoryTV;
+    private TextView mServingSuggestionTV;
+    private TextView mTastingNoteTV;
+    private TextView mOriginTV;
+    private TextView mReleasedOnTV;
+
+    private ImageButton mProductLoactionBtn;
 
     static public Map<String, Object> product;
+    private Bitmap loadedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,49 +49,93 @@ public class DetailActivity extends AppCompatActivity {
 
         mProductTitleTV = (TextView) findViewById(R.id.tv_product_title);
         mProductProducerTV = (TextView) findViewById(R.id.tv_product_producer);
-        mProductImageUrlIm = (Image) findViewById(R.id.im_product_image);
-        mProductLoactionBtn = (Button) findViewById(R.id.btn_product_location;
+        mPriceInCentsTV = (TextView) findViewById(R.id.tv_price_in_cents);
+        mAlcoholContentTV = (TextView) findViewById(R.id.tv_alcohol_content);
+        mPrimaryCategoryTV = (TextView) findViewById(R.id.tv_primary_category);
+        mSecondaryCategoryTV = (TextView) findViewById(R.id.tv_secondary_category);
+        mServingSuggestionTV = (TextView) findViewById(R.id.tv_serving_suggestion);
+        mTastingNoteTV = (TextView) findViewById(R.id.tv_tasting_note);
+        mOriginTV = (TextView) findViewById(R.id.tv_origin);
+        mReleasedOnTV = (TextView) findViewById(R.id.tv_released_on);
 
-        showProduct(name, producer);
+        mProductImageUrlIm = (ImageView) findViewById(R.id.im_product_image);
+        mProductLoactionBtn = (ImageButton) findViewById(R.id.btn_product_location);
 
+        // Setup universal image loader
+        DisplayImageOptions displayImageOptions = new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .build();
+        ImageLoaderConfiguration imageLoaderConfiguration = new ImageLoaderConfiguration.Builder(
+                getApplicationContext())
+                .defaultDisplayImageOptions(displayImageOptions)
+                .build();
+        ImageLoader.getInstance().init(imageLoaderConfiguration);
+
+        showProduct(product);
+    }
+
+    public void goToLocation(View v) {
+        setContentView(R.layout.activity_maps);
     }
 
     public void showProduct(Map<String, Object> product) {
 
-        final String name = (product.containsKey("name") && (product.get("name") instanceof String)) ? (String)product.get("name") : "Onbekend";
-        final String producer = (product.containsKey("producer_name") && (product.get("producer_name") instanceof String)) ? (String)product.get("producer_name") : "Onbekend";
-        final String imageURL = (product.containsKey("image_url") && (product.get("image_url") instanceof String)) ? (String)product.get("image_url") : null;
+        final String name               = (product.containsKey("name") && (product.get("name") instanceof String)) ? (String)product.get("name") : "Unknown";
+        final String primaryCategory    = (product.containsKey("primary_category") && (product.get("primary_category") instanceof String)) ? (String)product.get("primary_category") : "Unknown";
+        final String secondaryCategory  = (product.containsKey("secondary_category") && (product.get("secondary_category") instanceof String)) ? (String)product.get("secondary_category") : "Unknown";
+        final String servingSuggestion  = (product.containsKey("serving_suggestion") && (product.get("serving_suggestion") instanceof String)) ? (String)product.get("serving_suggestion") : "Unknown";
+        final String tastingNote        = (product.containsKey("tasting_note") && (product.get("tasting_note") instanceof String)) ? (String)product.get("tasting_note") : "Unknown";
+        final String origin             = (product.containsKey("origin") && (product.get("origin") instanceof String)) ? (String)product.get("origin") : "Unknown";
+        final String releasedOn         = (product.containsKey("released_on") && (product.get("released_on") instanceof String)) ? (String)product.get("released_on") : "Unknown";
+        final String producer           = (product.containsKey("producer_name") && (product.get("producer_name") instanceof String)) ? (String)product.get("producer_name") : "Unknown";
+        final Integer priceInCents      = (product.containsKey("price_in_cents") && (product.get("price_in_cents") instanceof Integer)) ? (Integer) product.get("price_in_cents") : null;
+        if (priceInCents == null) {
+            mPriceInCentsTV.setText("Unknown");
+        } else {
+            double d = priceInCents/100;
+            DecimalFormat df = new DecimalFormat(",##");
+            mPriceInCentsTV.setText("$" + df.format(d));
+        }
 
+        final Integer alcoholContent = (product.containsKey("alcohol_content") && (product.get("alcohol_content") instanceof Integer)) ? (Integer) product.get("alcohol_content") : null;
+        if(alcoholContent == null) {
+            mAlcoholContentTV.setText( "Unknown");
+        } else {
+            double d = alcoholContent/100;
+            DecimalFormat df = new DecimalFormat(",##");
+            mAlcoholContentTV.setText(df.format(d) + "%");
+        }
+
+        final String imageURL = (product.containsKey("image_url") && (product.get("image_url") instanceof String)) ? (String)product.get("image_url") : null;
         if (imageURL != null) {
             ImageLoader.getInstance().loadImage(imageURL, new ImageLoadingListener() {
                 @Override
-                public void onLoadingStarted(String imageUri, View view) {
-                    // TODO: 6/15/17 loader show code here
-                }
+                public void onLoadingStarted(String imageUri, View view) {}
 
                 @Override
-                public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
-                    complete(name, producer);
-                }
+                public void onLoadingFailed(String imageUri, View view, FailReason failReason) {}
 
                 @Override
                 public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-                    complete(name, producer);
                     BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), loadedImage);
                     mProductImageUrlIm.setImageDrawable(bitmapDrawable);
                 }
 
                 @Override
-                public void onLoadingCancelled(String imageUri, View view) {
-                    complete(name, producer);
-                }
+                public void onLoadingCancelled(String imageUri, View view) {}
             });
         } else {
-            complete(name, producer);
+            mProductImageUrlIm.setImageResource(R.drawable.none_bottle);
         }
 
         mProductTitleTV.setText(name);
-        mProductProducerTV.setText(String.format("Producer: %s", producer));
-
+        mProductProducerTV.setText(producer);
+        mPrimaryCategoryTV.setText(primaryCategory);
+        mSecondaryCategoryTV.setText(secondaryCategory);
+        mServingSuggestionTV.setText(servingSuggestion);
+        mTastingNoteTV.setText(tastingNote);
+        mOriginTV.setText(origin);
+        mReleasedOnTV.setText(releasedOn);
     }
 }
--- app/src/main/java/com/nickderonde/bottletime/MainActivity.java
+++ app/src/main/java/com/nickderonde/bottletime/MainActivity.java
@@ -1,13 +1,16 @@
 package com.nickderonde.bottletime;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaPlayer;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
+import android.media.audiofx.Visualizer;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -32,18 +35,20 @@ public class MainActivity extends AppCompatActivity implements NetworkUtils.Netw
     private ImageButton mSearchResultImageUrlIB;
     private MediaPlayer popSoundMP;
     private Button mGetBottleBtn;
-    private Map<String, Object> currentProduct;
+//    private MainView mMainView;
+    public Map<String, Object> currentProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        mMainView =(MainView) findViewById(R.id.activity_main);
+
         mSearchResultTitleTV = (TextView) findViewById(R.id.tv_random_bottle_title);
         mSearchResultProducerTV = (TextView) findViewById(R.id.tv_random_bottle_producer);
         mSearchResultImageUrlIB = (ImageButton) findViewById(R.id.ib_random_bottle_image);
         mGetBottleBtn = (Button) findViewById(R.id.button_get_bottle);
-
         popSoundMP = MediaPlayer.create(this, R.raw.pop);
 
         // Setup universal image loader
@@ -56,7 +61,15 @@ public class MainActivity extends AppCompatActivity implements NetworkUtils.Netw
                 .defaultDisplayImageOptions(displayImageOptions)
                 .build();
         ImageLoader.getInstance().init(imageLoaderConfiguration);
+
+//        setupSharedPreferences();
+//        setupPermissions();
     }
+
+//    public void setupSharedPreferences() {
+//        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        mMainView.(sharedPreferences.getBoolean("play_pop", true));
+//    }
 
     /**
      * Onclicklistener for shuffle button
@@ -72,6 +85,13 @@ public class MainActivity extends AppCompatActivity implements NetworkUtils.Netw
         NetworkUtils.makeLcboQuery();
     }
 
+
+    /**
+     * Go to detail activity when button is clicked
+     * and pass the product to DetailActivity
+     *
+     * @param v
+     */
     public void detailButtonHandler(View v) {
 
         //get product when no product is available
@@ -86,20 +106,27 @@ public class MainActivity extends AppCompatActivity implements NetworkUtils.Netw
     }
 
 
+    /**
+     * Array of the prodyct
+     * filter out the name, producer name and the imageurl
+     * Also do some checks and load all data after the image is loaded
+     * with the onconplete method
+     * @param product
+     */
     @Override
     public void randomProduct(Map<String, Object> product) {
 
         currentProduct = product;
 
-        final String name = (product.containsKey("name") && (product.get("name") instanceof String)) ? (String)product.get("name") : "Onbekend";
-        final String producer = (product.containsKey("producer_name") && (product.get("producer_name") instanceof String)) ? (String)product.get("producer_name") : "Onbekend";
+        final String name = (product.containsKey("name") && (product.get("name") instanceof String)) ? (String)product.get("name") : "Unknown";
+        final String producer = (product.containsKey("producer_name") && (product.get("producer_name") instanceof String)) ? (String)product.get("producer_name") : "Unknown";
         final String imageURL = (product.containsKey("image_url") && (product.get("image_url") instanceof String)) ? (String)product.get("image_url") : null;
 
         if (imageURL != null) {
             ImageLoader.getInstance().loadImage(imageURL, new ImageLoadingListener() {
                 @Override
                 public void onLoadingStarted(String imageUri, View view) {
-                    // TODO: 6/15/17 loader show code here
+                    //loader show code here
                 }
 
                 @Override
@@ -120,33 +147,54 @@ public class MainActivity extends AppCompatActivity implements NetworkUtils.Netw
                 }
             });
         } else {
+            mSearchResultImageUrlIB.setImageResource(R.drawable.none_bottle);
             complete(name, producer);
         }
     }
 
+
+    /**
+     * Complete method
+     *
+     * @param name
+     * @param producer
+     */
     private void complete(String name, String producer) {
         mSearchResultTitleTV.setText(name);
         mSearchResultProducerTV.setText(String.format("Producer: %s", producer));
         mGetBottleBtn.setEnabled(true);
-        popSoundMP.start();
+        playPop();
         Toast.makeText(MainActivity.this , "Served", Toast.LENGTH_SHORT).show();
     }
 
-    //show options menu in actionbar
+    /**
+     * Play sound Method
+     */
+    private void playPop(){
+        popSoundMP.start();
+    }
+
+    /**
+     * put options menu in actionbar
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
         return true;
     }
 
-    // start settings activity
+    /**
+     * start settings activity
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId()==R.id.action_settings) {
-            Intent settingsActivity = new Intent(this, SettingsActivity.class);
-            startActivity(settingsActivity);
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent SettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(SettingsActivity);
+            return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
